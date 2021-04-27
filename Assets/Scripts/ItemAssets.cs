c20503a144bbe41223ffd6a777ad7fa0285248dd
FrostDistrict <jordan.scott1396@gmail.com>
2021-04-27 08:51:43 -0400

    Create ItemAssets.

---
+++ Assets/Scripts/ItemAssets.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAssets : MonoBehaviour
+{
+    
+    public static ItemAssets Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Sprite hoeSprite;
+    public Sprite scytheSprite;
+    public Sprite WateringCanSprite;
+    a
+}
