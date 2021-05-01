0ae92dfffd5bbeb4291344f72837501883d4ff13
FrostDistrict <jordan.scott1396@gmail.com>
2021-05-01 19:36:08 -0400

    New inventory look. Trying to add click events, etc.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -7,58 +7,9 @@ using UnityEngine;
 public class Item
 {
 
-    public enum ItemType
-    {
-        Hoe,
-        Scythe,
-        WaterBucket,
-        
-        Watermelon,
-        //seeds
-        BeanSeed,
-        BlueberrySeed,
-        CabbageSeed,
-        CarrotSeed,
-        CauliflowerSeed,
-        CelerySeed,
-        CornSeed,
-        EggplantSeed,
-        GrapeSeed,
-        LeakSeed,
-        OnionSeed,
-        PepperSeed,
-        PineappleSeed,
-        PumpkinSeed,
-        RockmelonSeed,
-        SquashSeed,
-        StrawberrySeed,
-        TomatoSeed,        
-        TurnipSeed,
-        WatermelonSeed
-    }
-
-    public enum ItemClass
-    {
-        Tools,
-        Produce,
-        Seeds
-    }
-
     public ItemType itemType;
     public ItemClass itemClass;
     public int amount;
-
-
-    public Sprite GetSprite()
-    {
-        switch (itemType)
-        {
-            case ItemType.Hoe: return ItemAssets.Instance.hoeSprite;
-            case ItemType.Scythe: return ItemAssets.Instance.scytheSprite;
-            case ItemType.WaterBucket: return ItemAssets.Instance.waterBucketSprite;
-            default: return ItemAssets.Instance.hoeSprite;
-        }
-    }
 
     public Color GetColor()
     {
@@ -81,5 +32,114 @@ public class Item
             default:
                 return false;
         }
+    }
+    
+    public Sprite GetSprite()
+    {
+        switch (itemType)
+        {
+            case ItemType.Hoe: return ItemAssets.Instance.hoeSprite;
+            case ItemType.Scythe: return ItemAssets.Instance.scytheSprite;
+            case ItemType.WaterBucket: return ItemAssets.Instance.waterBucketSprite;
+            
+            case ItemType.Bean: return ItemAssets.Instance.bean;
+            case ItemType.Blueberry: return ItemAssets.Instance.blueberry;
+            case ItemType.Cabbage: return ItemAssets.Instance.cabbage;
+            case ItemType.Carrot: return ItemAssets.Instance.carrot;
+            case ItemType.Cauliflower: return ItemAssets.Instance.cauliflower;
+            case ItemType.Celery: return ItemAssets.Instance.celery;
+            case ItemType.Corn: return ItemAssets.Instance.corn;
+            case ItemType.Eggplant: return ItemAssets.Instance.eggplant;
+            case ItemType.Grape: return ItemAssets.Instance.grape;
+            case ItemType.Leak: return ItemAssets.Instance.leak;
+            case ItemType.Onion: return ItemAssets.Instance.onion;
+            case ItemType.Pepper: return ItemAssets.Instance.pepper;
+            case ItemType.Pineapple: return ItemAssets.Instance.pineapple;
+            case ItemType.Pumpkin: return ItemAssets.Instance.pumpkin;
+            case ItemType.Rockmelon: return ItemAssets.Instance.rockmelon;
+            case ItemType.Squash: return ItemAssets.Instance.squash;
+            case ItemType.Strawberry: return ItemAssets.Instance.strawberry;
+            case ItemType.Tomato: return ItemAssets.Instance.tomato;
+            case ItemType.Turnip: return ItemAssets.Instance.turnip;
+            case ItemType.Watermelon: return ItemAssets.Instance.watermelon;
+            
+            case ItemType.BeanSeed: return ItemAssets.Instance.bean;
+            case ItemType.BlueberrySeed: return ItemAssets.Instance.blueberry;
+            case ItemType.CabbageSeed: return ItemAssets.Instance.cabbage;
+            case ItemType.CarrotSeed: return ItemAssets.Instance.carrot;
+            case ItemType.CauliflowerSeed: return ItemAssets.Instance.cauliflower;
+            case ItemType.CelerySeed: return ItemAssets.Instance.celery;
+            case ItemType.CornSeed: return ItemAssets.Instance.corn;
+            case ItemType.EggplantSeed: return ItemAssets.Instance.eggplant;
+            case ItemType.GrapeSeed: return ItemAssets.Instance.grape;
+            case ItemType.LeakSeed: return ItemAssets.Instance.leak;
+            case ItemType.OnionSeed: return ItemAssets.Instance.onion;
+            case ItemType.PepperSeed: return ItemAssets.Instance.pepper;
+            case ItemType.PineappleSeed: return ItemAssets.Instance.pineapple;
+            case ItemType.PumpkinSeed: return ItemAssets.Instance.pumpkin;
+            case ItemType.RockmelonSeed: return ItemAssets.Instance.rockmelon;
+            case ItemType.SquashSeed: return ItemAssets.Instance.squash;
+            case ItemType.StrawberrySeed: return ItemAssets.Instance.strawberry;
+            case ItemType.TomatoSeed: return ItemAssets.Instance.tomato;
+            case ItemType.TurnipSeed: return ItemAssets.Instance.turnip;
+            case ItemType.WatermelonSeed: return ItemAssets.Instance.watermelon;
+            default: return ItemAssets.Instance.hoeSprite;
+        }
+    }
+    
+    public enum ItemClass
+    {
+        Tools,
+        Produce,
+        Seeds
+    }
+    
+    public enum ItemType
+    {
+        Hoe,
+        Scythe,
+        WaterBucket,
+        //produce
+        Bean,
+        Blueberry,
+        Cabbage,
+        Carrot,
+        Cauliflower,
+        Celery,
+        Corn,
+        Eggplant,
+        Grape,
+        Leak,
+        Onion,
+        Pepper,
+        Pineapple,
+        Pumpkin,
+        Rockmelon,
+        Squash,
+        Strawberry,
+        Tomato,
+        Turnip,
+        Watermelon,
+        //seeds
+        BeanSeed,
+        BlueberrySeed,
+        CabbageSeed,
+        CarrotSeed,
+        CauliflowerSeed,
+        CelerySeed,
+        CornSeed,
+        EggplantSeed,
+        GrapeSeed,
+        LeakSeed,
+        OnionSeed,
+        PepperSeed,
+        PineappleSeed,
+        PumpkinSeed,
+        RockmelonSeed,
+        SquashSeed,
+        StrawberrySeed,
+        TomatoSeed,        
+        TurnipSeed,
+        WatermelonSeed
     }
 }
--- Assets/Scripts/UI_Inventory.cs
+++ Assets/Scripts/UI_Inventory.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UI_Inventory : MonoBehaviour
@@ -18,6 +20,11 @@ public class UI_Inventory : MonoBehaviour
 
     public GameObject go;
 
+    private Dictionary<Transform, Item> _inventorySlots = new Dictionary<Transform, Item>();
+    
+    float itemSlotCellSize = 60f;
+    float itemSlotOffset = 30f;
+
     //[SerializeField] private GameObject _itemSlotContainer;
 
     private void Awake()
@@ -29,19 +36,79 @@ public class UI_Inventory : MonoBehaviour
         _inventory = inventory;
         
         go = Instantiate(gameObject);
-        _canvas = go.transform.GetChild(0);
-        
+        CreateSlots();
+
         itemSlotContainer = go.transform.GetChild(0).Find("itemSlotContainer");
         itemSlotTemplate = itemSlotContainer.Find("itemSlotTemplate");
 
         inventory.OnItemListChanged += InventoryOnItemListChanged;
         
-        RefreshInventoryItems();
+        //RefreshInventoryItems();
     }
 
     private void InventoryOnItemListChanged(object sender, EventArgs e)
     {
-        RefreshInventoryItems();
+        //RefreshInventoryItems();
+    }
+
+    private void CreateSlots()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                var itemSlotObj = Instantiate(itemSlotTemplate, itemSlotContainer);
+                itemSlotObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(j * itemSlotOffset, i * itemSlotOffset);
+                
+                AddEvent(itemSlotObj, EventTriggerType.PointerEnter, delegate {OnPointerEnter(itemSlotObj);});
+                AddEvent(itemSlotObj, EventTriggerType.PointerExit, delegate {OnPointerExit(itemSlotObj);});
+                AddEvent(itemSlotObj, EventTriggerType.BeginDrag, delegate {OnDragStart(itemSlotObj);});
+                AddEvent(itemSlotObj, EventTriggerType.EndDrag, delegate {OnDragEnd(itemSlotObj);});
+                AddEvent(itemSlotObj, EventTriggerType.Drag, delegate {OnDrag(itemSlotObj);});
+                AddEvent(itemSlotObj, EventTriggerType.PointerClick, delegate {OnPointerClick(itemSlotObj);});
+                
+                _inventorySlots.Add(itemSlotObj, null);
+            }
+        }
+    }
+    
+    private void AddEvent(Transform slotTransform, EventTriggerType type, UnityAction<BaseEventData> action)
+    {
+        EventTrigger trigger = slotTransform.GetComponent<EventTrigger>();
+        var eventTrigger = new EventTrigger.Entry();
+        eventTrigger.eventID = type;
+        eventTrigger.callback.AddListener(action);
+        trigger.triggers.Add(eventTrigger);
+    }
+
+    private void OnPointerClick(Transform slotTransform)
+    {
+        
+    }
+
+    private void OnPointerEnter(Transform slotTransform)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnPointerExit(Transform slotTransform)
+    {
+        
+    }
+
+    private void OnDragStart(Transform slotTransform)
+    {
+        
+    }
+    
+    private void OnDragEnd(Transform slotTransform)
+    {
+        
+    }
+    
+    private void OnDrag(Transform slotTransform)
+    {
+        
     }
 
     private void RefreshInventoryItems()
@@ -54,8 +121,6 @@ public class UI_Inventory : MonoBehaviour
         
         int x = -2;
         int y = 1;
-        float itemSlotCellSize = 60f;
-        float itemSlotOffset = 30f;
         foreach (var item in _inventory.GetItemList())
         {
             RectTransform itemSlotRectTransform = Instantiate(itemSlotContainer, _canvas).GetComponent<RectTransform>();
