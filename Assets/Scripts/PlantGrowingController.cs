831955210da4a908c22245755d2faa573991abba
Stephanie <ishnewalon@hotmail.com>
2021-05-01 15:55:59 -0400

    Makes plants grow(change sprite) only if a new day

--- Assets/Scripts/PlantGrowingController.cs
+++ Assets/Scripts/PlantGrowingController.cs
@@ -5,40 +5,51 @@ using UnityEngine;
 
 public class PlantGrowingController : MonoBehaviour
 {
-    private int _currrentPlantSprite = 0;
+    private int _currrentGrowthStage = 0;
     private Enum _plantType;
+    private const int _DaysNeededForMaxGrowth = 4;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.Instance.onGameStateChanged.AddListener(HandleGameStateChanged);
+    }
+
+    private void HandleGameStateChanged(GameManager.GameState currentState, GameManager.GameState previousState)
+    {
+        if (previousState == GameManager.GameState.RUNNING && currentState == GameManager.GameState.GOTOBED)
+        {
+            _currrentGrowthStage++;
+            if ((_currrentGrowthStage < 4) && (_currrentGrowthStage >= 0))
+            {
+                MakePlantGrow();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.activeSelf == true && _currrentPlantSprite == 0 && GameManager.Instance._cropSeedEnum.Contains(GameManager.Instance._currentHeldItem))
+        if (gameObject.activeSelf == true && _currrentGrowthStage == 0 && GameManager.Instance._cropSeedEnum.Contains(GameManager.Instance._currentHeldItem))
         {
             _plantType = GameManager.Instance._currentHeldItem;
             GetComponent<SpriteRenderer>().sprite =
-                GameManager.Instance._plantSprites[_plantType][_currrentPlantSprite];
+                GameManager.Instance._plantSprites[_plantType][_currrentGrowthStage];
                 
         }
     }
 
     private void OnMouseDown()
     {
-        _currrentPlantSprite++;
-        print(_currrentPlantSprite);
-        if ((GameManager.Instance._cropSeedEnum.Contains(GameManager.Instance._currentHeldItem)) && (_currrentPlantSprite < 4) && (_currrentPlantSprite >= 0))
+        if (!GameManager.Instance._cropSeedEnum.Contains(GameManager.Instance._currentHeldItem))
         {
-            GetComponent<SpriteRenderer>().sprite =
-                GameManager.Instance._plantSprites[_plantType][_currrentPlantSprite];
+            
         }
-        else if (_currrentPlantSprite >= 4)
-        {
-            gameObject.SetActive(false);
-            _currrentPlantSprite = 0;
-        }
+    }
+    
+    private void MakePlantGrow()
+    {
+        GetComponent<SpriteRenderer>().sprite =
+            GameManager.Instance._plantSprites[_plantType][_currrentGrowthStage];
     }
 }
--- Assets/Scripts/TerrainManager.cs
+++ Assets/Scripts/TerrainManager.cs
@@ -11,6 +11,7 @@ public class TerrainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(this.gameObject);
         _plantGO = gameObject.transform.GetChild(0).gameObject;
     }
 
